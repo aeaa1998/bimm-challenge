1166ea08053e11fb492e52d12c76c4b1c5ccb5bc
Augusto <augusto@quilolatam.com>
2024-02-06 19:19:25 -0600

    Initial Commit

---
+++ BIMM/BIMMApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIMMApp.swift
+//  BIMM
+//
+//  Created by Augusto Alonso on 6/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BIMMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
